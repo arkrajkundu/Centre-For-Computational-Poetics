367e62a6834a1ffd5f58cfd097251244408012db
arkrajkundu <arkraj.oggy@gmail.com>
2024-05-21 16:40:47 +0530

    Edited copyright message, updated technical contributions

--- src/components/TechnicalContributions/TechnicalContributions.jsx
+++ src/components/TechnicalContributions/TechnicalContributions.jsx
@@ -8,6 +8,7 @@ const TechnicalContributions = () => {
       <hr />
       <h2>Technical Contributions</h2>
       <Contributions tool={"Website (Development & Maintenance)"} contributor={"Aditya Srivastava [BTECH/10195/22], Aman Choudhary [BTECH/10269/22], Arkraj Kundu [BTECH/10232/22], Shresth Bhattacharjee [BTECH/10246/22], Vishal Deora [BTECH/10263/22]"} />
+      <Contributions tool={"Logo Design"} contributor={"Abhyuday Pandey [BTECH/10174/22], Aditi [MBA/10067/22]"} />
       <Contributions tool={"Text2Mātrā"} contributor={"Komal Naaz, Niraj Kumar Singh"} />
       <Contributions tool={"RPaGen"} contributor={"Komal Naaz, Niraj Kumar Singh"} />
       <Contributions tool={"RMaGen"} contributor={"Komal Naaz, Niraj Kumar Singh"} />
