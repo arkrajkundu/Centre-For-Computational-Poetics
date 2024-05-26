a32090ad45f117e1ddaac9a52b08785b1fb21c9d
ADITYA SRIVASTAVA <adityasrivastava@192.168.1.3>
2024-05-26 12:31:42 +0530

    added hamburger menu along with resposniveness

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,47 +1,112 @@
-/* eslint-disable no-unused-vars */
-
-import React, { useContext } from 'react'
-import './Navbar.css'
-import Button from '../Button/Button'
-import Searchbar from '../Searchbar/Searchbar'
-import { Link, useNavigate } from 'react-router-dom'
-import { LoginContext } from '../../contexts/LoginContext'
-import { assets } from '../../assets/assets'
+import React, { useContext, useState } from 'react';
+import './Navbar.css';
+import Button from '../Button/Button';
+import { Link, useNavigate } from 'react-router-dom';
+import { LoginContext } from '../../contexts/LoginContext';
+import { assets } from '../../assets/assets';
 
 const Navbar = ({ setShowLogin }) => {
-
   const navigate = useNavigate();
   const { token, setToken } = useContext(LoginContext);
+  const [showMenu, setShowMenu] = useState(false);
 
   const logout = () => {
-    localStorage.removeItem("token");
-    setToken("");
-    navigate("/")
-  }
+    localStorage.removeItem('token');
+    setToken('');
+    navigate('/');
+  };
+
+  const handleMenuClick = () => {
+    setShowMenu(!showMenu);
+  };
 
   return (
     <div className='navbar'>
-      <ul>
-        <Link to='/'><Button text='Home' /></Link>
-        <Link to='/about-us'><Button text='About Us' /></Link>
-        <Link to='/collaborations'><Button text='Collaborations' /></Link>
-        <Link to='/faculty-team'><Button text='Faculty Team' /></Link>
-        <Link to='/students-team'><Button text='Students Team' /></Link>
-        <Link to='/news'><Button text='News' /></Link>
-        <Link to='/events'><Button text='Events' /></Link>
-        <Link to='/publications'><Button text='Publications' /></Link>
-        <Link to='/home/RDL'><Button text='Resources' /></Link>
-        <Link to='/contact-us'><Button text='Contact Us' /></Link>
-        {!token ? <Button text='Log In' onClick={setShowLogin} /> : <Button text='Log Out' onClick={logout} />}
-
-
-        {/* <Button text='Log In' onClick={setShowLogin} />
-        {!token ? <Button text='Log In' onClick={setShowLogin} />
-          :
-          <Button text='Log Out' onClick={logout} />} */}
-      </ul>
+      <div className='desktop-menu'>
+        <ul className='desktop-menu-texts'>
+          <Link to='/'>
+            <Button text='Home' />
+          </Link>
+          <Link to='/about-us'>
+            <Button text='About Us' />
+          </Link>
+          <Link to='/collaborations'>
+            <Button text='Collaborations' />
+          </Link>
+          <Link to='/faculty-team'>
+            <Button text='Faculty Team' />
+          </Link>
+          <Link to='/students-team'>
+            <Button text='Students Team' />
+          </Link>
+          <Link to='/news'>
+            <Button text='News' />
+          </Link>
+          <Link to='/events'>
+            <Button text='Events' />
+          </Link>
+          <Link to='/publications'>
+            <Button text='Publications' />
+          </Link>
+          <Link to='/home/RDL'>
+            <Button text='Resources' />
+          </Link>
+          <Link to='/contact-us'>
+            <Button text='Contact Us' />
+          </Link>
+          {!token ? (
+            <Button text='Log In' onClick={setShowLogin} />
+          ) : (
+            <Button text='Log Out' onClick={logout} />
+          )}
+        </ul>
+      </div>
+      <div className='mobile-menu'>
+        <div className='hamburger-menu' onClick={handleMenuClick}>
+          MENU
+        </div>
+        {showMenu && (
+          <ul className='mobile-nav'>
+            <Link to='/'>
+              <Button text='Home' />
+            </Link>
+            <Link to='/about-us'>
+              <Button text='About Us' />
+            </Link>
+            <Link to='/collaborations'>
+              <Button text='Collaborations' />
+            </Link>
+            <Link to='/faculty-team'>
+              <Button text='Faculty Team' />
+            </Link>
+            <Link to='/students-team'>
+              <Button text='Students Team' />
+            </Link>
+            <Link to='/news'>
+              <Button text='News' />
+            </Link>
+            <Link to='/events'>
+              <Button text='Events' />
+            </Link>
+            <Link to='/publications'>
+              <Button text='Publications' />
+            </Link>
+            <Link to='/home/RDL'>
+              <Button text='Resources' />
+            </Link>
+            <Link to='/contact-us'>
+              <Button text='Contact Us' />
+            </Link>
+            {!token ? (
+              <Button text='Log In' onClick={setShowLogin} />
+            ) : (
+              <Button text='Log Out' onClick={logout} />
+            )}
+          </ul>
+        )}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
